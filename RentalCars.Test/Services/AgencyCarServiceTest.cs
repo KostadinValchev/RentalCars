0dad40e04511abccf381b663581b0a0041981071
crespo <ktvalchev>
2018-02-28 22:32:40 +0200

    Added tests for AgencyCarServiceTest

---
+++ RentalCars.Test/Services/AgencyCarServiceTest.cs
@@ -0,0 +1,146 @@
+﻿namespace RentalCars.Test.Services
+{
+    using Data;
+    using Data.Models;
+    using FluentAssertions;
+    using RentalCars.Services.Agency.Implementations;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class AgencyCarServiceTest
+    {
+        const int one = 1;
+        const int two = 2;
+        const int hundred = 100;
+        const int twoHundred = 200;
+        const string agencyName = "FakeAgency";
+        const string user = "FakeUser";
+
+        private RentalCarsDbContext db;
+
+        public AgencyCarServiceTest()
+        {
+            Tests.Initialize();
+            db = Tests.GetDatabase();
+        }
+
+        [Fact]
+        public async Task FindShouldReturnResultWithFilter()
+        {
+            //Arrange
+            var fakeCarOne = new Car { Id = one };
+            db.AddRange(fakeCarOne);
+            await db.SaveChangesAsync();
+
+            var agencyCarService = new AgencyCarService(db);
+            //Act
+            var result = await agencyCarService.FindByIdAsync(1);
+
+            //Assert
+            result
+                .Id
+                .Should()
+                .Be(1);
+        }
+
+        [Fact]
+        public async Task FindAsyncShouldReturnCorrectAllCarsByFilterAndOrder()
+        {
+            //Arrange
+            var agency = new Agency()
+            {
+                Id = one,
+                Name = agencyName,
+                User = new User() { Id = user },
+            };
+
+            var fakeCarOne = new Car()
+            {
+                Id = one,
+                Price = hundred,
+                Agency = agency,
+                City = new City() { Id = one },
+            };
+
+            var fakeCarTwo = new Car()
+            {
+                Id = two,
+                Price = twoHundred,
+                Agency = agency,
+                City = new City() { Id = one },
+            };
+
+            this.db.Cars.AddRange(fakeCarOne, fakeCarTwo);
+            await this.db.SaveChangesAsync();
+
+            var agencyCarService = new AgencyCarService(db);
+            //Act
+            var result = await agencyCarService.AllCarsAsync(agencyName);
+
+            //Assert
+            result
+                .Should()
+                .Contain(r => r.Agency.Name == agencyName);
+
+            result.Should().
+                Match(r => r.ElementAt(0).Price == hundred);
+
+
+        }
+
+        [Fact]
+        public async Task FindAsyncShouldReturnCorrectAllCarsWhichAreReturnedFromUserByFilterAndOrder()
+        {
+            //Arrange
+            var agency = new Agency()
+            {
+                Id = one,
+                Name = agencyName,
+                User = new User() { Id = user },
+            };
+
+            var fakeCarOne = new Car()
+            {
+                Id = one,
+                Price = hundred,
+                Agency = agency,
+                City = new City() { Id = one },
+                ReturnDate = DateTime.MinValue,
+                IsReserved = true
+            };
+
+            var fakeCarTwo = new Car()
+            {
+                Id = two,
+                Price = twoHundred,
+                Agency = agency,
+                City = new City() { Id = one },
+                ReturnDate = DateTime.MaxValue,
+                IsReserved = true
+            };
+
+            this.db.Cars.AddRange(fakeCarOne, fakeCarTwo);
+            await this.db.SaveChangesAsync();
+
+            var agencyCarService = new AgencyCarService(db);
+
+            //Act
+            var result = await agencyCarService.AllReturnedCarsAsync(agencyName);
+            //Assert
+
+            result
+                .Should()
+                .Contain(r => r.Agency.Name == agencyName);
+
+            foreach (var car in result)
+            {
+                car.ReturnDate.Should().BeBefore(DateTime.UtcNow);
+                car.IsReserved.Should().Be(true);
+            }
+
+            result.Should().BeInAscendingOrder(r => r.Price);
+        }
+    }
+}
---
+++ RentalCars.Test/Tests.cs
@@ -0,0 +1,31 @@
+﻿namespace RentalCars.Test
+{
+    using AutoMapper;
+    using Microsoft.EntityFrameworkCore;
+    using RentalCars.Data;
+    using RentalCars.Web.Infrastructure.Mapping;
+    using System;
+
+    public class Tests
+    {
+        private static bool testInitialized = false;
+
+        public static void Initialize()
+        {
+            if (!testInitialized)
+            {
+                Mapper.Initialize(config => config.AddProfile<AutoMapperProfile>());
+                testInitialized = true;
+            }
+        }
+
+        public static RentalCarsDbContext GetDatabase()
+        {
+            var dbOptions = new DbContextOptionsBuilder<RentalCarsDbContext>()
+                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                 .Options;
+
+            return new RentalCarsDbContext(dbOptions);
+        }
+    }
+}
